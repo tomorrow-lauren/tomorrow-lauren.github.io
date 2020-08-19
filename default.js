9a20999a3b389530946ad739a58c044c11c498a8
Lauren Reid <lauren.g.reid@gmail.com>
2020-08-18 21:33:37 -0700

    index rehaul

--- default.js
+++ default.js
@@ -1,20 +1,10 @@
-$(document).ready(function () {
-  $("#SFQ").on("click", "i", function(){
-    event.preventDefault();
-    $("#SFQ-portfolio").toggleClass("hidden");
-    $("#sf-icon").toggleClass("fa fa-arrow-down");
-    $("#sf-icon").toggleClass("fa fa-arrow-up");
-  });
-  $("#grouvie").on("click", "i", function(){
-    event.preventDefault();
-    $("#G-portfolio").toggleClass("hidden");
-    $("#g-icon").toggleClass("fa fa-arrow-down");
-    $("#g-icon").toggleClass("fa fa-arrow-up");
-  });
-  $("#codepen").on("click", "i", function(){
-    event.preventDefault();
-    $("#C-portfolio").toggleClass("hidden");
-    $("#c-icon").toggleClass("fa fa-arrow-down");
-    $("#c-icon").toggleClass("fa fa-arrow-up");
-  });
+$("#accordion").on("shown.bs.collapse", e => {
+  $("html, body").animate(
+    {
+      scrollTop: $(e.target)
+        .prev()
+        .offset().top
+    },
+    400
+  );
 });
